03969ad765698ee09e8d97019db379c506325062
zhoujun <572459439@qq.com>
2020-05-16 17:24:16 +0800

    rnn输出通道由cnn class获取

--- torchocr/networks/architectures/rec_model.py
+++ torchocr/networks/architectures/rec_model.py
@@ -4,6 +4,7 @@
 from torch import nn
 
 from torchocr.networks.backbones.rec_mobilenet_v3 import MobileNetV3
+from torchocr.networks.backbones.rec_resnet_vd import ResNet
 from torchocr.networks.necks.rec_sequence_decoder import SequenceDecoder
 from torchocr.networks.heads.rec_ctc_head import CTC
 
@@ -11,13 +12,14 @@ from torchocr.networks.heads.rec_ctc_head import CTC
 class RECModel(nn.Module):
     def __init__(self, in_channels, **kwargs):
         super().__init__()
-        self.backbone = MobileNetV3(in_channels)
+        self.backbone = ResNet(in_channels, 34)
         d = {'type': 'rnn'}
-        self.neck = SequenceDecoder(480, **d)
+        self.neck = SequenceDecoder(self.backbone.out_channels, **d)
         self.head = CTC(384, 37)
 
     def forward(self, x):
         x = self.backbone(x)
+        print(x.shape)
         x = self.neck(x)
         x = self.head(x)
         return x
@@ -25,8 +27,9 @@ class RECModel(nn.Module):
 
 if __name__ == '__main__':
     import torch
-    x = torch.zeros(1,3,32,320)
+
+    x = torch.zeros(1, 3, 32, 320)
     model = RECModel(3)
-    y= model(x)
+    y = model(x)
     print(y.shape)
-    torch.save(model.state_dict(),'1,pth')
+    torch.save(model.state_dict(), 'rec_model.pth')
