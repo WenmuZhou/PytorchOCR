df8b26f47035e9da3073ee6bbfef690b81acaa65
novio <744351893@qq.com>
2020-05-18 10:56:12 +0800

    重名网络的文件名，并且能够加载paddle的预训练模型

---
+++ torchocr/networks/heads/RecCTCHead.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from collections import OrderedDict
+
+import torch
+from torch import nn
+
+
+class CTC(nn.Module):
+    def __init__(self, in_channels, n_class, **kwargs):
+        super().__init__()
+        self.fc = nn.Linear(in_channels, n_class)
+
+    def load_3rd_state_dict(self, _3rd_name, _state):
+        to_load_state_dict = OrderedDict()
+        if _3rd_name == 'paddle':
+            to_load_state_dict['fc.weight'] = torch.Tensor(_state['ctc_fc_w_attr'].T)
+            to_load_state_dict['fc.bias'] = torch.Tensor(_state['ctc_fc_b_attr'])
+            self.load_state_dict(to_load_state_dict)
+        else:
+            pass
+
+    def forward(self, x):
+        return self.fc(x)
