6687037c130632a0b6143f8aa35a7826772c25fc
zhoujun <572459439@qq.com>
2020-05-19 17:31:25 +0800

    添加det的mb3和resnet backbone

---
+++ torchocr/networks/backbones/DetMobilenetV3.py
@@ -0,0 +1,237 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from _collections import OrderedDict
+import torch
+from torch import nn
+from torchocr.networks.CommonModules import HSwish, HardSigmoid
+
+__all__ = ['MobileNetV3']
+
+
+class ConvBNACT(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, groups=1, act=None):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
+                              stride=stride, padding=padding, groups=groups,
+                              bias=False)
+        self.bn = nn.BatchNorm2d(out_channels)
+        if act == 'relu':
+            self.act = nn.ReLU()
+        elif act == 'hard_swish':
+            self.act = HSwish()
+        elif act is None:
+            self.act = None
+
+    def load_3rd_state_dict(self, _3rd_name, _state, _name_prefix):
+        to_load_state_dict = OrderedDict()
+        if _3rd_name == 'paddle':
+            to_load_state_dict['conv.weight'] = torch.Tensor(_state[f'{_name_prefix}_weights'])
+            to_load_state_dict['bn.weight'] = torch.Tensor(_state[f'{_name_prefix}_bn_scale'])
+            to_load_state_dict['bn.bias'] = torch.Tensor(_state[f'{_name_prefix}_bn_offset'])
+            to_load_state_dict['bn.running_mean'] = torch.Tensor(_state[f'{_name_prefix}_bn_mean'])
+            to_load_state_dict['bn.running_var'] = torch.Tensor(_state[f'{_name_prefix}_bn_variance'])
+            self.load_state_dict(to_load_state_dict)
+        else:
+            pass
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        if self.act is not None:
+            x = self.act(x)
+        return x
+
+
+class SEBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, ratio=4):
+        super().__init__()
+        num_mid_filter = out_channels // ratio
+        self.pool = nn.AdaptiveAvgPool2d(1)
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=num_mid_filter, kernel_size=1, bias=True)
+        self.relu1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(in_channels=num_mid_filter, kernel_size=1, out_channels=out_channels, bias=True)
+        self.relu2 = HardSigmoid()
+
+    def load_3rd_state_dict(self, _3rd_name, _state, _name_prefix):
+        to_load_state_dict = OrderedDict()
+        if _3rd_name == 'paddle':
+            to_load_state_dict['conv1.weight'] = torch.Tensor(_state[f'{_name_prefix}_1_weights'])
+            to_load_state_dict['conv2.weight'] = torch.Tensor(_state[f'{_name_prefix}_2_weights'])
+            to_load_state_dict['conv1.bias'] = torch.Tensor(_state[f'{_name_prefix}_1_offset'])
+            to_load_state_dict['conv2.bias'] = torch.Tensor(_state[f'{_name_prefix}_2_offset'])
+            self.load_state_dict(to_load_state_dict)
+        else:
+            pass
+
+    def forward(self, x):
+        attn = self.pool(x)
+        attn = self.conv1(attn)
+        attn = self.relu1(attn)
+        attn = self.conv2(attn)
+        attn = self.relu2(attn)
+        return x * attn
+
+
+class ResidualUnit(nn.Module):
+    def __init__(self, num_in_filter, num_mid_filter, num_out_filter, stride, kernel_size, act=None, use_se=False):
+        super().__init__()
+        self.conv0 = ConvBNACT(in_channels=num_in_filter, out_channels=num_mid_filter, kernel_size=1, stride=1,
+                               padding=0, act=act)
+
+        self.conv1 = ConvBNACT(in_channels=num_mid_filter, out_channels=num_mid_filter, kernel_size=kernel_size,
+                               stride=stride,
+                               padding=int((kernel_size - 1) // 2), act=act, groups=num_mid_filter)
+        if use_se:
+            self.se = SEBlock(in_channels=num_mid_filter, out_channels=num_mid_filter)
+        else:
+            self.se = None
+
+        self.conv2 = ConvBNACT(in_channels=num_mid_filter, out_channels=num_out_filter, kernel_size=1, stride=1,
+                               padding=0)
+        self.not_add = num_in_filter != num_out_filter or stride != 1
+
+    def load_3rd_state_dict(self, _3rd_name, _state, _convolution_index):
+        if _3rd_name == 'paddle':
+            self.conv0.load_3rd_state_dict(_3rd_name, _state, f'conv{_convolution_index}_expand')
+            self.conv1.load_3rd_state_dict(_3rd_name, _state, f'conv{_convolution_index}_depthwise')
+            if self.se is not None:
+                self.se.load_3rd_state_dict(_3rd_name, _state, f'conv{_convolution_index}_se')
+            self.conv2.load_3rd_state_dict(_3rd_name, _state, f'conv{_convolution_index}_linear')
+        else:
+            pass
+        pass
+
+    def forward(self, x):
+        y = self.conv0(x)
+        y = self.conv1(y)
+        if self.se is not None:
+            y = self.se(y)
+        y = self.conv2(y)
+        if not self.not_add:
+            y = x + y
+        return y
+
+
+class MobileNetV3(nn.Module):
+    def __init__(self, in_channels, **kwargs):
+        """
+        the MobilenetV3 backbone network for detection module.
+        Args:
+            params(dict): the super parameters for build network
+        """
+        super().__init__()
+        self.scale = kwargs.get('scale', 0.5)
+        model_name = kwargs.get('model_name', 'large')
+        self.inplanes = 16
+        if model_name == "large":
+            self.cfg = [
+                # k, exp, c,  se,     nl,  s,
+                [3, 16, 16, False, 'relu', 1],
+                [3, 64, 24, False, 'relu', 2],
+                [3, 72, 24, False, 'relu', 1],
+                [5, 72, 40, True, 'relu', 2],
+                [5, 120, 40, True, 'relu', 1],
+                [5, 120, 40, True, 'relu', 1],
+                [3, 240, 80, False, 'hard_swish', 2],
+                [3, 200, 80, False, 'hard_swish', 1],
+                [3, 184, 80, False, 'hard_swish', 1],
+                [3, 184, 80, False, 'hard_swish', 1],
+                [3, 480, 112, True, 'hard_swish', 1],
+                [3, 672, 112, True, 'hard_swish', 1],
+                [5, 672, 160, True, 'hard_swish', 2],
+                [5, 960, 160, True, 'hard_swish', 1],
+                [5, 960, 160, True, 'hard_swish', 1],
+            ]
+            self.cls_ch_squeeze = 960
+            self.cls_ch_expand = 1280
+        elif model_name == "small":
+            self.cfg = [
+                # k, exp, c,  se,     nl,  s,
+                [3, 16, 16, True, 'relu', 2],
+                [3, 72, 24, False, 'relu', 2],
+                [3, 88, 24, False, 'relu', 1],
+                [5, 96, 40, True, 'hard_swish', 2],
+                [5, 240, 40, True, 'hard_swish', 1],
+                [5, 240, 40, True, 'hard_swish', 1],
+                [5, 120, 48, True, 'hard_swish', 1],
+                [5, 144, 48, True, 'hard_swish', 1],
+                [5, 288, 96, True, 'hard_swish', 2],
+                [5, 576, 96, True, 'hard_swish', 1],
+                [5, 576, 96, True, 'hard_swish', 1],
+            ]
+            self.cls_ch_squeeze = 576
+            self.cls_ch_expand = 1280
+        else:
+            raise NotImplementedError("mode[" + model_name +
+                                      "_model] is not implemented!")
+
+        supported_scale = [0.35, 0.5, 0.75, 1.0, 1.25]
+        assert self.scale in supported_scale, \
+            "supported scale are {} but input scale is {}".format(supported_scale, self.scale)
+
+        scale = self.scale
+        inplanes = self.inplanes
+        cfg = self.cfg
+        cls_ch_squeeze = self.cls_ch_squeeze
+        # conv1
+        self.conv1 = ConvBNACT(in_channels=in_channels,
+                               out_channels=self.make_divisible(inplanes * scale),
+                               kernel_size=3,
+                               stride=2,
+                               padding=1,
+                               groups=1,
+                               act='hard_swish')
+        i = 0
+        inplanes = self.make_divisible(inplanes * scale)
+        self.stages = nn.ModuleList()
+        block_list = []
+        for layer_cfg in cfg:
+            if layer_cfg[5] == 2 and i > 2:
+                self.stages.append(nn.Sequential(*block_list))
+                block_list = []
+            block = ResidualUnit(num_in_filter=inplanes,
+                                 num_mid_filter=self.make_divisible(scale * layer_cfg[1]),
+                                 num_out_filter=self.make_divisible(scale * layer_cfg[2]),
+                                 act=layer_cfg[4],
+                                 stride=layer_cfg[5],
+                                 kernel_size=layer_cfg[0],
+                                 use_se=layer_cfg[3])
+            block_list.append(block)
+            inplanes = self.make_divisible(scale * layer_cfg[2])
+            i += 1
+        conv = ConvBNACT(
+            in_channels=inplanes,
+            out_channels=self.make_divisible(scale * cls_ch_squeeze),
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            groups=1,
+            act='hard_swish')
+        block_list.append(conv)
+        self.stages.append(nn.Sequential(*block_list))
+
+    def make_divisible(self, v, divisor=8, min_value=None):
+        if min_value is None:
+            min_value = divisor
+        new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+        if new_v < 0.9 * v:
+            new_v += divisor
+        return new_v
+
+    def forward(self, x):
+        x = self.conv1(x)
+        out = []
+        for stage in self.stages:
+            x = stage(x)
+            out.append(x)
+        return out
+
+
+if __name__ == '__main__':
+    x = torch.zeros(1, 3, 640, 640)
+    model = MobileNetV3(3)
+    y = model(x)
+    for i in y:
+        print(i.shape)
+    torch.save(model.state_dict(),'1.pth')
---
+++ torchocr/networks/backbones/DetResNet34vd.py
@@ -0,0 +1,262 @@
+# copyright (c) 2020 PaddlePaddle Authors. All Rights Reserve.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from collections import OrderedDict
+import torch
+from torch import nn
+
+from torchocr.networks.CommonModules import HSwish
+
+__all__ = ["ResNet"]
+
+
+class ConvBNACT(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, groups=1, act=None):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
+                              stride=stride, padding=padding, groups=groups,
+                              bias=False)
+        self.bn = nn.BatchNorm2d(out_channels)
+        if act == 'relu':
+            self.act = nn.ReLU()
+        elif act == 'hard_swish':
+            self.act = HSwish()
+        elif act is None:
+            self.act = None
+
+    def load_3rd_state_dict(self, _3rd_name, _state, _name_prefix):
+        to_load_state_dict = OrderedDict()
+        if _3rd_name == 'paddle':
+            to_load_state_dict['conv.weight'] = torch.Tensor(_state[f'{_name_prefix}_weights'])
+            if _name_prefix == 'conv1':
+                bn_name = f'bn_{_name_prefix}'
+            else:
+                bn_name = f'bn{_name_prefix[3:]}'
+            to_load_state_dict['bn.weight'] = torch.Tensor(_state[f'{bn_name}_scale'])
+            to_load_state_dict['bn.bias'] = torch.Tensor(_state[f'{bn_name}_offset'])
+            to_load_state_dict['bn.running_mean'] = torch.Tensor(_state[f'{bn_name}_mean'])
+            to_load_state_dict['bn.running_var'] = torch.Tensor(_state[f'{bn_name}_variance'])
+            self.load_state_dict(to_load_state_dict)
+        else:
+            pass
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        if self.act is not None:
+            x = self.act(x)
+        return x
+
+
+class ConvBNACTWithPool(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, groups=1, act=None):
+        super().__init__()
+        self.pool = nn.AvgPool2d(kernel_size=2, stride=2, padding=0, ceil_mode=True)
+
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=1,
+                              padding=(kernel_size - 1) // 2,
+                              groups=groups,
+                              bias=False)
+        self.bn = nn.BatchNorm2d(out_channels)
+        if act is None:
+            self.act = None
+        else:
+            self.act = nn.ReLU()
+
+    def load_3rd_state_dict(self, _3rd_name, _state, _name_prefix):
+        to_load_state_dict = OrderedDict()
+        if _3rd_name == 'paddle':
+            to_load_state_dict['conv.weight'] = torch.Tensor(_state[f'{_name_prefix}_weights'])
+            if _name_prefix == 'conv1':
+                bn_name = f'bn_{_name_prefix}'
+            else:
+                bn_name = f'bn{_name_prefix[3:]}'
+            to_load_state_dict['bn.weight'] = torch.Tensor(_state[f'{bn_name}_scale'])
+            to_load_state_dict['bn.bias'] = torch.Tensor(_state[f'{bn_name}_offset'])
+            to_load_state_dict['bn.running_mean'] = torch.Tensor(_state[f'{bn_name}_mean'])
+            to_load_state_dict['bn.running_var'] = torch.Tensor(_state[f'{bn_name}_variance'])
+            self.load_state_dict(to_load_state_dict)
+        else:
+            pass
+
+    def forward(self, x):
+        x = self.pool(x)
+        x = self.conv(x)
+        x = self.bn(x)
+        if self.act is not None:
+            x = self.act(x)
+        return x
+
+
+class ShortCut(nn.Module):
+    def __init__(self, in_channels, out_channels, stride, name, if_first=False):
+        super().__init__()
+        assert name is not None, 'shortcut must have name'
+
+        self.name = name
+        if in_channels != out_channels or stride != 1:
+            if if_first:
+                self.conv = ConvBNACT(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=stride,
+                                      padding=0, groups=1, act=None)
+            else:
+                self.conv = ConvBNACTWithPool(in_channels=in_channels, out_channels=out_channels, kernel_size=1,
+                                              groups=1, act=None)
+        elif if_first:
+            self.conv = ConvBNACT(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=stride,
+                                  padding=0, groups=1, act=None)
+        else:
+            self.conv = None
+
+    def load_3rd_state_dict(self, _3rd_name, _state):
+        if _3rd_name == 'paddle':
+            if self.conv:
+                self.conv.load_3rd_state_dict(_3rd_name, _state, self.name)
+        else:
+            pass
+
+    def forward(self, x):
+        if self.conv is not None:
+            x = self.conv(x)
+        return x
+
+
+class BottleneckBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride, if_first, name):
+        super().__init__()
+        assert name is not None, 'bottleneck must have name'
+        self.name = name
+        self.conv0 = ConvBNACT(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=1, padding=0,
+                               groups=1, act='relu')
+        self.conv1 = ConvBNACT(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=stride,
+                               padding=1, groups=1, act='relu')
+        self.conv2 = ConvBNACT(in_channels=out_channels, out_channels=out_channels * 4, kernel_size=1, stride=1,
+                               padding=1, groups=1, act=None)
+        self.shortcut = ShortCut(in_channels=in_channels, out_channels=out_channels * 4, stride=stride,
+                                 if_first=if_first, name=f'{name}_branch1')
+        self.relu = nn.ReLU()
+
+    def load_3rd_state(self, _3rd_name, _state):
+        self.conv0.load_3rd_state_dict(_3rd_name, _state, f'{self.name}_branch2a')
+        self.conv1.load_3rd_state_dict(_3rd_name, _state, f'{self.name}_branch2b')
+        self.conv2.load_3rd_state_dict(_3rd_name, _state, f'{self.name}_branch2c')
+        self.shortcut.load_3rd_state_dict(_3rd_name, _state)
+
+    def forward(self, x):
+        y = self.conv0(x)
+        y = self.conv1(y)
+        y = self.conv2(y)
+        y = y + self.shortcut(x)
+        return self.relu(y)
+
+
+class BasicBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride, if_first, name):
+        super().__init__()
+        assert name is not None, 'block must have name'
+        self.name = name
+
+        self.conv0 = ConvBNACT(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=stride,
+                               padding=1, groups=1, act='relu')
+        self.conv1 = ConvBNACT(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1,
+                               groups=1, act=None)
+        self.shortcut = ShortCut(in_channels=in_channels, out_channels=out_channels, stride=stride,
+                                 name=f'{name}_branch1', if_first=if_first, )
+        self.relu = nn.ReLU()
+
+    def load_3rd_state_dict(self, _3rd_name, _state):
+        if _3rd_name == 'paddle':
+            self.conv0.load_3rd_state_dict(_3rd_name, _state, f'{self.name}_branch2a')
+            self.conv1.load_3rd_state_dict(_3rd_name, _state, f'{self.name}_branch2b')
+            self.shortcut.load_3rd_state_dict(_3rd_name, _state)
+        else:
+            pass
+
+    def forward(self, x):
+        y = self.conv0(x)
+        y = self.conv1(y)
+        y = y + self.shortcut(x)
+        return self.relu(y)
+
+
+class ResNet(nn.Module):
+    def __init__(self, in_channels, layers, **kwargs):
+        """
+        the Resnet backbone network for detection module.
+        Args:
+            params(dict): the super parameters for network build
+        """
+        super().__init__()
+        supported_layers = {
+            18: {'depth': [2, 2, 2, 2], 'block_class': BasicBlock},
+            34: {'depth': [3, 4, 6, 3], 'block_class': BasicBlock},
+            50: {'depth': [3, 4, 6, 3], 'block_class': BottleneckBlock},
+            101: {'depth': [3, 4, 23, 3], 'block_class': BottleneckBlock},
+            152: {'depth': [3, 8, 36, 3], 'block_class': BottleneckBlock},
+            200: {'depth': [3, 12, 48, 3], 'block_class': BottleneckBlock}
+        }
+        assert layers in supported_layers, \
+            "supported layers are {} but input layer is {}".format(supported_layers, layers)
+        depth = supported_layers[layers]['depth']
+        block_class = supported_layers[layers]['block_class']
+
+        num_filters = [64, 128, 256, 512]
+        self.conv1 = nn.Sequential(
+            ConvBNACT(in_channels=in_channels, out_channels=32, kernel_size=3, stride=2, padding=1, act='relu'),
+            ConvBNACT(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1, act='relu'),
+            ConvBNACT(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1, act='relu')
+        )
+        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.stages = nn.ModuleList()
+        in_ch = 64
+        for block_index in range(len(depth)):
+            block_list = []
+            for i in range(depth[block_index]):
+                if layers >= 50:
+                    if layers in [101, 152, 200] and block_index == 2:
+                        if i == 0:
+                            conv_name = "res" + str(block_index + 2) + "a"
+                        else:
+                            conv_name = "res" + str(block_index + 2) + "b" + str(i)
+                    else:
+                        conv_name = "res" + str(block_index + 2) + chr(97 + i)
+                else:
+                    conv_name = f'res{str(block_index + 2)}{chr(97 + i)}'
+                block_list.append(block_class(in_channels=in_ch, out_channels=num_filters[block_index], stride=2 if i == 0 and block_index != 0 else 1,
+                                              if_first=block_index == i == 0, name=conv_name))
+                in_ch = num_filters[block_index]
+            self.stages.append(nn.Sequential(*block_list))
+        self.out_channels = in_ch
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.pool1(x)
+        out = []
+        for stage in self.stages:
+            x = stage(x)
+            out.append(x)
+        return out
+
+
+if __name__ == '__main__':
+    x = torch.zeros(1, 3, 640, 640)
+    model = ResNet(3,34)
+    y = model(x)
+    for i in y:
+        print(i.shape)
+    torch.save(model.state_dict(),'1.pth')
