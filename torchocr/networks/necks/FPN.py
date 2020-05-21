ada17aa05216bdcc728b769661a4d8b1526ea2c1
zhoujun <572459439@qq.com>
2020-05-21 16:03:51 +0800

    添加FPN

--- torchocr/networks/necks/FPN.py
+++ torchocr/networks/necks/FPN.py
@@ -1,156 +1,51 @@
-# -*- coding:utf-8 -*-
-# @author :adolf
-from collections import OrderedDict
-
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/21 13:50
+# @Author  : zhoujun
+import torch
+from torch import nn
 import torch.nn.functional as F
-from torch import nn, Tensor
-
-from torch.jit.annotations import Tuple, List, Dict
 
 
-class FeaturePyramidNetwork(nn.Module):
-    """
-    Arguments:
-        in_channels_list (list[int]): number of channels for each feature map that
-            is passed to the module
-        out_channels (int): number of channels of the FPN representation
-        extra_blocks (ExtraFPNBlock or None): if provided, extra operations will
-            be performed. It is expected to take the fpn features, the original
-            features and the names of the original features as input, and returns
-            a new list of feature maps and their corresponding names
-    """
-    def __init__(self, in_channels_list, out_channels, extra_blocks=None):
-        super(FeaturePyramidNetwork, self).__init__()
-        self.inner_blocks = nn.ModuleList()
-        self.layer_blocks = nn.ModuleList()
-        for in_channels in in_channels_list:
-            if in_channels == 0:
-                raise ValueError("in_channels=0 is currently not supported")
-            inner_block_module = nn.Conv2d(in_channels, out_channels, 1)
-            layer_block_module = nn.Conv2d(out_channels, out_channels, 3, padding=1)
-            self.inner_blocks.append(inner_block_module)
-            self.layer_blocks.append(layer_block_module)
+class FPN(nn.Module):
+    def __init__(self, in_channels, out_channels=256, **kwargs):
+        super().__init__()
+        assert len(in_channels) == 4
+        self.out_channels = out_channels
+        self.reduce_c5 = nn.Conv2d(in_channels=in_channels[3], out_channels=self.out_channels, kernel_size=1, bias=False)
+        self.reduce_c4 = nn.Conv2d(in_channels=in_channels[2], out_channels=self.out_channels, kernel_size=1, bias=False)
+        self.reduce_c3 = nn.Conv2d(in_channels=in_channels[1], out_channels=self.out_channels, kernel_size=1, bias=False)
+        self.reduce_c2 = nn.Conv2d(in_channels=in_channels[0], out_channels=self.out_channels, kernel_size=1, bias=False)
 
-        # initialize parameters now to avoid modifying the initialization of top_blocks
-        for m in self.children():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_uniform_(m.weight, a=1)
-                nn.init.constant_(m.bias, 0)
-
-        if extra_blocks is not None:
-            assert isinstance(extra_blocks, ExtraFPNBlock)
-        self.extra_blocks = extra_blocks
-
-    def get_result_from_inner_blocks(self, x, idx):
-        num_blocks = 0
-        for m in self.inner_blocks:
-            num_blocks += 1
-        if idx < 0:
-            idx += num_blocks
-        i = 0
-        out = x
-        for module in self.inner_blocks:
-            if i == idx:
-                out = module(x)
-            i += 1
-        return out
-
-    def get_result_from_layer_blocks(self, x, idx):
-        num_blocks = 0
-        for m in self.layer_blocks:
-            num_blocks += 1
-        if idx < 0:
-            idx += num_blocks
-        i = 0
-        out = x
-        for module in self.layer_blocks:
-            if i == idx:
-                out = module(x)
-            i += 1
-        return out
+        self.smooth_p5 = nn.Conv2d(in_channels=out_channels, out_channels=self.out_channels // 4, kernel_size=3, padding=1, bias=False)
+        self.smooth_p4 = nn.Conv2d(in_channels=out_channels, out_channels=self.out_channels // 4, kernel_size=3, padding=1, bias=False)
+        self.smooth_p3 = nn.Conv2d(in_channels=out_channels, out_channels=self.out_channels // 4, kernel_size=3, padding=1, bias=False)
+        self.smooth_p2 = nn.Conv2d(in_channels=out_channels, out_channels=self.out_channels // 4, kernel_size=3, padding=1, bias=False)
 
     def forward(self, x):
-        """
-        Computes the FPN for a set of feature maps.
+        c2, c3, c4, c5 = x
+        in5 = self.reduce_c5(c5)
+        in4 = self.reduce_c4(c4)
+        in3 = self.reduce_c3(c3)
+        in2 = self.reduce_c2(c2)
 
-        Arguments:
-            x (OrderedDict[Tensor]): feature maps for each feature level.
+        out4 = self._upsample_add(in5, in4)
+        out3 = self._upsample_add(out4, in3)
+        out2 = self._upsample_add(out3, in2)
 
-        Returns:
-            results (OrderedDict[Tensor]): feature maps after FPN layers.
-                They are ordered from highest resolution first.
-        """
-        # unpack OrderedDict into two lists for easier handling
-        names = list(x.keys())
-        x = list(x.values())
+        p5 = self.smooth_p5(in5)
+        p4 = self.smooth_p4(out4)
+        p3 = self.smooth_p3(out3)
+        p2 = self.smooth_p2(out2)
 
-        last_inner = self.get_result_from_inner_blocks(x[-1], -1)
-        results = []
-        results.append(self.get_result_from_layer_blocks(last_inner, -1))
-
-        for idx in range(len(x) - 2, -1, -1):
-            inner_lateral = self.get_result_from_inner_blocks(x[idx], idx)
-            feat_shape = inner_lateral.shape[-2:]
-            inner_top_down = F.interpolate(last_inner, size=feat_shape, mode="nearest")
-            last_inner = inner_lateral + inner_top_down
-            results.insert(0, self.get_result_from_layer_blocks(last_inner, idx))
-
-        if self.extra_blocks is not None:
-            results, names = self.extra_blocks(results, x, names)
-
-        # make it back an OrderedDict
-        out = OrderedDict([(k, v) for k, v in zip(names, results)])
-
+        out = self._upsample_cat(p2, p3, p4, p5)
         return out
 
+    def _upsample_add(self, x, y):
+        return F.interpolate(x, size=y.size()[2:]) + y
 
-class ExtraFPNBlock(nn.Module):
-    """
-    Base class for the extra block in the FPN.
-
-    Arguments:
-        results (List[Tensor]): the result of the FPN
-        x (List[Tensor]): the original feature maps
-        names (List[str]): the names for each one of the
-            original feature maps
-
-    Returns:
-        results (List[Tensor]): the extended set of results
-            of the FPN
-        names (List[str]): the extended set of names for the results
-    """
-    def forward(self, results, x, names):
-        pass
-
-
-class LastLevelMaxPool(ExtraFPNBlock):
-    """
-    Applies a max_pool2d on top of the last feature map
-    """
-    def forward(self, x, y, names):
-        names.append("pool")
-        x.append(F.max_pool2d(x[-1], 1, 2, 0))
-        return x, names
-
-
-class LastLevelP6P7(ExtraFPNBlock):
-    """
-    This module is used in RetinaNet to generate extra layers, P6 and P7.
-    """
-    def __init__(self, in_channels, out_channels):
-        super(LastLevelP6P7, self).__init__()
-        self.p6 = nn.Conv2d(in_channels, out_channels, 3, 2, 1)
-        self.p7 = nn.Conv2d(out_channels, out_channels, 3, 2, 1)
-        for module in [self.p6, self.p7]:
-            nn.init.kaiming_uniform_(module.weight, a=1)
-            nn.init.constant_(module.bias, 0)
-        self.use_P5 = in_channels == out_channels
-
-    def forward(self, p, c, names):
-        p5, c5 = p[-1], c[-1]
-        x = p5 if self.use_P5 else c5
-        p6 = self.p6(x)
-        p7 = self.p7(F.relu(p6))
-        p.extend([p6, p7])
-        names.extend(["p6", "p7"])
-        return p, names
+    def _upsample_cat(self, p2, p3, p4, p5):
+        h, w = p2.size()[2:]
+        p3 = F.interpolate(p3, size=(h, w))
+        p4 = F.interpolate(p4, size=(h, w))
+        p5 = F.interpolate(p5, size=(h, w))
+        return torch.cat([p2, p3, p4, p5], dim=1)
