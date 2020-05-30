579def185d680cf4da5b6b4d3f3e07594e0d1eac
Jinreejing <1033757005@qq.com>
2020-05-30 12:14:16 +0800

    调通loss函数

--- torchocr/networks/losses/CTCLoss.py
+++ torchocr/networks/losses/CTCLoss.py
@@ -8,10 +8,18 @@ class CTCLoss(nn.Module):
         super().__init__()
         self.loss_func = torch.nn.CTCLoss(blank=blank_idx, reduction=reduction)
 
-    def forward(self, pred, label):
+    def forward(self, pred, args):
+        label, label_length = args[0], args[1]
+
+        pred = pred.transpose(1, 0).log_softmax(2).detach().requires_grad_()
+
         labels = torch.IntTensor([])
+        label_size = []
         for j in range(label.size(0)):
-            labels = torch.cat((labels, label[j]), 0)
+            labels = torch.cat((labels, label[j][:label_length[j]]), 0)
+            label_size.append(label_length[j])
+
+        label_size = torch.IntTensor(label_size)
         output_size = torch.IntTensor([pred.size(0)] * int(pred.size(1)))
-        label_size = torch.IntTensor([label.size(1)] * int(label.size(0)))
+
         return self.loss_func(pred, labels, output_size, label_size)
