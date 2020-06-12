cebbcf6bb98935dd4a816b0dbaed6a2268e759f9
novio <744351893@qq.com>
2020-06-12 14:22:35 +0800

    调整在加载ctc的时候维度不一致的问题

--- torchocr/networks/heads/RecCTCHead.py
+++ torchocr/networks/heads/RecCTCHead.py
@@ -12,13 +12,15 @@ class CTC(nn.Module):
     def __init__(self, in_channels, n_class, **kwargs):
         super().__init__()
         self.fc = nn.Linear(in_channels, n_class)
+        self.n_class = n_class
 
     def load_3rd_state_dict(self, _3rd_name, _state):
         to_load_state_dict = OrderedDict()
         if _3rd_name == 'paddle':
-            to_load_state_dict['fc.weight'] = torch.Tensor(_state['ctc_fc_w_attr'].T)
-            to_load_state_dict['fc.bias'] = torch.Tensor(_state['ctc_fc_b_attr'])
-            self.load_state_dict(to_load_state_dict)
+            if _state['ctc_fc_b_attr'].size == self.n_class:
+                to_load_state_dict['fc.weight'] = torch.Tensor(_state['ctc_fc_w_attr'].T)
+                to_load_state_dict['fc.bias'] = torch.Tensor(_state['ctc_fc_b_attr'])
+                self.load_state_dict(to_load_state_dict)
         else:
             pass
 
