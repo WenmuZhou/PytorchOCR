fe595a20d681497a7855b349513188ad9ab3cf0d
zhoujun <572459439@qq.com>
2020-06-23 13:50:29 +0800

    修正train_loss记录错误的bug

--- tools/det_train.py
+++ tools/det_train.py
@@ -182,9 +182,9 @@ def train(net, optimizer, loss_func, train_loader, eval_loader, to_use_device,
     base_lr = optimizer.param_groups[0]['lr']
     try:
         start = time.time()
-        train_loss = 0.
         for epoch in range(_epoch, train_options['epochs']):  # traverse each epoch
             net.train()  # train mode
+            train_loss = 0.
             for i, batch_data in enumerate(train_loader):  # traverse each batch in the epoch
                 current_lr = adjust_learning_rate(optimizer, base_lr, epoch, train_options['epochs'], 0.9)
                 # 数据进行转换和丢到gpu
