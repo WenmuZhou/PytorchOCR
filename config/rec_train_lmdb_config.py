45cb17e73fe4b19b2d45f2036396b7a430478dac
zhoujun <572459439@qq.com>
2020-06-29 14:45:05 +0800

    标记device为不建议修改

--- config/rec_train_lmdb_config.py
+++ config/rec_train_lmdb_config.py
@@ -30,7 +30,7 @@ config.train_options = {
     'resume_from': '',  # 继续训练地址
     'third_party_name': '',  # 加载paddle模型可选
     'checkpoint_save_dir': f"./output/{config.exp_name}/checkpoint",  # 模型保存地址，log文件也保存在这里
-    'device': 'cuda:0',
+    'device': 'cuda:0',# 不建议修改
     'epochs': 200,
     'fine_tune_stage': ['backbone', 'neck', 'head'],
     'print_interval': 10,  # step为单位
