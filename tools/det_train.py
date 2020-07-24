ed9737b3d604d9a2fcb995639d3a9382bd6b1e5b
zhoujun <572459439@qq.com>
2020-07-24 23:45:57 +0800

    训练时，先保存模型再验证

--- tools/det_train.py
+++ tools/det_train.py
@@ -244,18 +244,18 @@ def train(net, optimizer, loss_func, train_loader, eval_loader, to_use_device,
             global_state['best_model'] = best_model
             global_state['global_step'] = global_step
             if (epoch + 1) % train_options['val_interval'] == 0:
-                # val
                 net_save_path = f"{train_options['checkpoint_save_dir']}/latest.pth"
                 save_checkpoint(net_save_path, net, optimizer, logger, cfg, global_state=global_state)
                 if train_options['ckpt_save_type'] == 'HighestAcc':
+                    # val
                     eval_dict = evaluate(net, eval_loader, to_use_device, logger, post_process, metric)
                     if eval_dict['hmean'] > best_model['hmean']:
                         best_model.update(eval_dict)
                         best_model['best_model_epoch'] = epoch
                         best_model['models'] = net_save_path
-                        shutil.copy(net_save_path, net_save_path.replace('latest', 'best'))
+                        shutil.copy(net_save_path, net_save_path.replace('latest.pth', 'best.pth'))
                 elif train_options['ckpt_save_type'] == 'FixedEpochStep' and epoch % train_options['ckpt_save_epoch'] == 0:
-                    shutil.copy(net_save_path, net_save_path.replace('latest', str(epoch)))
+                    shutil.copy(net_save_path, net_save_path.replace('latest.pth', f'{epoch}.pth'))
                 best_str = 'current best, '
                 for k, v in best_model.items():
                     best_str += '{}: {}, '.format(k, v)
