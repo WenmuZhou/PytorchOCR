37935be0dd6d5d837bdf8b3e22b62e4a66949418
zhoujun <572459439@qq.com>
2020-08-08 14:14:06 +0800

    更新FPN

--- torchocr/networks/necks/FPN.py
+++ torchocr/networks/necks/FPN.py
@@ -5,40 +5,61 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
+class ConvBnRelu(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros', inplace=True):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation,
+                              groups=groups, bias=bias, padding_mode=padding_mode)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=inplace)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        return x
 
 class FPN(nn.Module):
     def __init__(self, in_channels, out_channels=256, **kwargs):
+        """
+        :param in_channels: 基础网络输出的维度
+        :param kwargs:
+        """
         super().__init__()
-        assert len(in_channels) == 4
-        self.out_channels = out_channels
-        self.reduce_c5 = nn.Conv2d(in_channels=in_channels[3], out_channels=self.out_channels, kernel_size=1, bias=False)
-        self.reduce_c4 = nn.Conv2d(in_channels=in_channels[2], out_channels=self.out_channels, kernel_size=1, bias=False)
-        self.reduce_c3 = nn.Conv2d(in_channels=in_channels[1], out_channels=self.out_channels, kernel_size=1, bias=False)
-        self.reduce_c2 = nn.Conv2d(in_channels=in_channels[0], out_channels=self.out_channels, kernel_size=1, bias=False)
+        inplace = True
+        self.conv_out = out_channels
+        out_channels = out_channels // 4
+        # reduce layers
+        self.reduce_conv_c2 = ConvBnRelu(in_channels[0], out_channels, kernel_size=1, inplace=inplace)
+        self.reduce_conv_c3 = ConvBnRelu(in_channels[1], out_channels, kernel_size=1, inplace=inplace)
+        self.reduce_conv_c4 = ConvBnRelu(in_channels[2], out_channels, kernel_size=1, inplace=inplace)
+        self.reduce_conv_c5 = ConvBnRelu(in_channels[3], out_channels, kernel_size=1, inplace=inplace)
+        # Smooth layers
+        self.smooth_p4 = ConvBnRelu(out_channels, out_channels, kernel_size=3, padding=1, inplace=inplace)
+        self.smooth_p3 = ConvBnRelu(out_channels, out_channels, kernel_size=3, padding=1, inplace=inplace)
+        self.smooth_p2 = ConvBnRelu(out_channels, out_channels, kernel_size=3, padding=1, inplace=inplace)
 
-        self.smooth_p5 = nn.Conv2d(in_channels=out_channels, out_channels=self.out_channels // 4, kernel_size=3, padding=1, bias=False)
-        self.smooth_p4 = nn.Conv2d(in_channels=out_channels, out_channels=self.out_channels // 4, kernel_size=3, padding=1, bias=False)
-        self.smooth_p3 = nn.Conv2d(in_channels=out_channels, out_channels=self.out_channels // 4, kernel_size=3, padding=1, bias=False)
-        self.smooth_p2 = nn.Conv2d(in_channels=out_channels, out_channels=self.out_channels // 4, kernel_size=3, padding=1, bias=False)
+        self.conv = nn.Sequential(
+            nn.Conv2d(self.conv_out, self.conv_out, kernel_size=3, padding=1, stride=1),
+            nn.BatchNorm2d(self.conv_out),
+            nn.ReLU(inplace=inplace)
+        )
+        self.out_channels = self.conv_out
 
     def forward(self, x):
         c2, c3, c4, c5 = x
-        in5 = self.reduce_c5(c5)
-        in4 = self.reduce_c4(c4)
-        in3 = self.reduce_c3(c3)
-        in2 = self.reduce_c2(c2)
+        # Top-down
+        p5 = self.reduce_conv_c5(c5)
+        p4 = self._upsample_add(p5, self.reduce_conv_c4(c4))
+        p4 = self.smooth_p4(p4)
+        p3 = self._upsample_add(p4, self.reduce_conv_c3(c3))
+        p3 = self.smooth_p3(p3)
+        p2 = self._upsample_add(p3, self.reduce_conv_c2(c2))
+        p2 = self.smooth_p2(p2)
 
-        out4 = self._upsample_add(in5, in4)
-        out3 = self._upsample_add(out4, in3)
-        out2 = self._upsample_add(out3, in2)
-
-        p5 = self.smooth_p5(in5)
-        p4 = self.smooth_p4(out4)
-        p3 = self.smooth_p3(out3)
-        p2 = self.smooth_p2(out2)
-
-        out = self._upsample_cat(p2, p3, p4, p5)
-        return out
+        x = self._upsample_cat(p2, p3, p4, p5)
+        x = self.conv(x)
+        return x
 
     def _upsample_add(self, x, y):
         return F.interpolate(x, size=y.size()[2:]) + y
