b54462fe3dfd9286f321636fd8bc25a34170c455
WenmuZhou <572459439@qq.com>
2023-09-05 03:30:35 +0000

    del paddle

--- tools/infer/utility.py
+++ tools/infer/utility.py
@@ -1,31 +1,7 @@
-# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import argparse
-import os
-import sys
-import platform
 import cv2
 import numpy as np
-import paddle
 import torch.cuda
-from PIL import Image, ImageDraw, ImageFont
-import math
-from paddle import inference
-import time
-import random
-from ppocr.utils.logging import get_logger
 
 
 def str2bool(v):
