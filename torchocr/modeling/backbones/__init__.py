feaa9cd8e8d6c32c5859d551da5d29f55928ac08
zhangxinnan <zhangxin19870504@163.com>
2025-12-26 16:30:54 +0800

    to support PP-OCRv5-server-det

--- torchocr/modeling/backbones/__init__.py
+++ torchocr/modeling/backbones/__init__.py
@@ -7,8 +7,10 @@ def build_backbone(config, model_type):
         from .det_resnet_vd import ResNet_vd
         from .rec_lcnetv3 import PPLCNetV3
         from .rec_hgnet import PPHGNet_small
+        from .rec_pphgnetv2 import PPHGNetV2_B4
         support_dict = [
-            'MobileNetV3', 'ResNet_vd', 'PPLCNetV3', 'PPHGNet_small'
+            'MobileNetV3', 'ResNet_vd', 'PPLCNetV3', 'PPHGNet_small',
+            'PPHGNetV2_B4',
         ]
     elif model_type == "rec" or model_type == "cls":
         from .rec_mobilenet_v3 import MobileNetV3
